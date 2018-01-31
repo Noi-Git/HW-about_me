f2a5bb7cd1a184f9a40fb7e928a133255663efd1
Noi-Git <noi.patsin@gmail.com>
2018-01-31 00:15:59 -0800

    second commit

---
+++ assets/js/index-min.js
@@ -0,0 +1,1 @@
+const y=new Date;document.getElementById("copyright").innerHTML="Created by Noi Sinnang &copy; "+y.getFullYear();
---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+
+
+const y =new Date();
+document.getElementById("copyright").innerHTML = "Created by Noi Sinnang &copy; " + y.getFullYear();
